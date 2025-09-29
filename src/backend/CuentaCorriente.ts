2a27dd7e517594f02ed2b5eaacf46eb973edd62b
Alexander <diegooointer@gmail.com>
2025-09-28 20:35:15 -0500

    se agrego iniciar sesion en clientes y cuanta corriente, ahorros

---
+++ src/backend/CuentaCorriente.ts
@@ -0,0 +1,21 @@
+import { Cuenta } from "./Cuenta";
+
+export class CuentaCorriente extends Cuenta {
+  private limiteSobregiro: number = 500000;
+
+  constructor(numeroCuenta: string, saldoInicial: number = 0) {
+    super(numeroCuenta, saldoInicial);
+  }
+
+  realizarRetiro(monto: number) {
+    if (monto <= 0) return `El monto debe ser mayor a 0`;
+
+    if (monto > this.saldo + this.limiteSobregiro) {
+      return `Fondos insuficientes. El sobregiro máximo permitido es $${this.limiteSobregiro}`;
+    }
+
+    this.saldo -= monto;
+    this.historial.push(`Retiro (con sobregiro): $${monto}`);
+    return `Retiro de $${monto}, saldo actual: $${this.saldo}`;
+  }
+}
